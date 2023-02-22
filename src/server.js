e5aafab35740caeb66b2d153204b65178f50dacc
Finn <contact@bechinka.eu>
2023-02-22 06:41:45 +0100

    feat: add match scraping

--- src/server.js
+++ src/server.js
@@ -1,47 +1,51 @@
+const express = require("express");
+const path = require("path");
+const db = require("mongoose");
+const Match = require("./schemas/Match.js");
+
+const PORT = 3000;
+
+db.connect("mongodb://mongo:2717/esportal-scraper");
+
+function log(message) {
+  console.log("[" + new Date().toISOString() + "] " + message);
+}
+
 async function get_live_matches() {
-  res = await fetch("https://esportal.com/api/live_games/list?_=1677021440843&region_id=0", {
-    credentials: "include",
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0",
-      Accept: "*/*",
-      "Accept-Language": "en-US,de;q=0.7,en;q=0.3",
-      "Sec-Fetch-Dest": "empty",
-      "Sec-Fetch-Mode": "cors",
-      "Sec-Fetch-Site": "same-origin",
-    },
-    referrer: "https://esportal.com/en/matchmaking",
+  const timestamp = Math.floor(new Date().getTime() / 1000);
+  log(timestamp);
+  res = await fetch(`https://esportal.com/api/live_games/list?region_id=0`, {
     method: "GET",
     mode: "cors",
   });
   body = await res.json();
-  console.log(body);
-  return res;
+  return body;
 }
 
-// get_live_matches();
+async function store_new_matches() {
+  const live_matches = await get_live_matches();
+  for (const live_match of live_matches) {
+    if (!live_match.map_id || (await Match.exists({ id: live_match.id }))) {
+      continue;
+    }
+    const match = await Match.create({
+      ...live_match,
+    });
+  }
+  const all_matches = await Match.find({});
+  log("INFO: current match count: " + all_matches.length);
+}
 
-// const { PrometheusDriver } = require("prometheus-query");
+store_new_matches();
+setInterval(store_new_matches, 5 * 1000 * 60);
 
-// const prom = new PrometheusDriver({
-//   endpoint: "http://192.168.2.2:9090",
-//   baseURL: "/api/v1/", // default value
-// });
+const app = express();
 
-// const q =
-//   'avg_over_time(speedtest_jitter_miliseconds{instance="192.168.2.2:9101",job="raspi-speedtest"}[24y])';
-// async function haha() {
-//   prom
-//     .instantQuery(q)
-//     .then((res) => {
-//       const series = res.result;
-//       series.forEach((serie) => {
-//         console.log("Serie:", serie.metric.toString());
-//         console.log("Time:", serie.value.time);
-//         console.log("Value:", serie.value.value);
-//       });
-//     })
-//     .catch(console.error("wtf"));
-// }
+app.get("/", async (req, res) => {
+  // res.sendFile(path.join(__dirname, "/pages/index.html"));
+  const all_matches = await Match.find({});
+  res.json(all_matches);
+});
 
-// haha();
+app.listen(PORT);
+log("INFO: listening on port " + PORT);
