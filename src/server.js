ca8d8305bb4d3694b280217c7f2b687188cf56e6
Finn <contact@bechinka.eu>
2023-02-26 01:30:12 +0100

    feat: set strict query to suppress warning

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const map_translation = JSON.parse(fs.readFileSync("./src/misc/maps.json"));
 
 const PORT = 3000;
 
+db.set("strictQuery", true);
 db.connect(process.env.MONGODB_CONNECTION_STRING);
 
 function log(message) {
